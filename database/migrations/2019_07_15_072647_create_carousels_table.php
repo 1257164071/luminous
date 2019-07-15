2db9c85a3f5a5530c613255d74fd361f8df892dd
NightDays <1257164071@qq.com>
2019-07-15 15:42:56 +0800

    数据迁移表生成

---
+++ database/migrations/2019_07_15_072647_create_carousels_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarouselsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carousels', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->dateTime('create_date');
+            $table->string('remark');
+            $table->string('title',400);
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carousels');
+    }
+}
